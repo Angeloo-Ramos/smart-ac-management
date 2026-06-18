6ee267c46821612cb9593c0ed9365c5c08a7db88
Angeloo-Ramos <angelloramos007@gmail.com>
2026-06-18 05:03:46 -0300

    feat: add manual time and date configuration

--- Menu.cpp
+++ Menu.cpp
@@ -24,17 +24,7 @@ void Menu::execOpcao(int option) {
   switch (option) {
 
     case 1:
-
-      Serial.print("Hora: ");
-
-      Serial.print(now.hour(), DEC);
-      Serial.print(':');
-
-      Serial.print(now.minute(), DEC);
-      Serial.print(':');
-
-      Serial.println(now.second(), DEC);
-
+      mostrarHora();
       break;
 
     case 2:
@@ -58,9 +48,7 @@ void Menu::execOpcao(int option) {
       break;
 
     case 3:
-
-      Serial.println("Funcao 'ajustar hora' ainda nao implementada.");
-
+      ajustarHora();
       break;
 
     case 4:
@@ -75,4 +63,110 @@ void Menu::execOpcao(int option) {
 
       break;
   }
+}
+
+void Menu::mostrarHora(){  //case 1
+  DateTime agora = rtc.now();
+
+  Serial.print("Hora gravada: ");
+
+  if (agora.hour() < 10) Serial.print('0');
+  Serial.print(agora.hour());
+
+  Serial.print(':');
+
+  if (agora.minute() < 10) Serial.print('0');
+  Serial.print(agora.minute());
+
+  Serial.print(':');
+
+  if (agora.second() < 10) Serial.print('0');
+  Serial.println(agora.second());
+}
+
+int Menu::lerInteiro(const char* mensagem){
+  Serial.println(mensagem);
+
+  while(!Serial.available()){}
+
+  int valor = Serial.parseInt();
+
+  while(Serial.available()){
+    Serial.read();
+  }
+
+  return valor;
+}
+
+void Menu::ajustarData(){
+  while(!Serial){}
+
+  Serial.println();
+  Serial.println("===== SISTEMAS RTC =====");
+
+  if(!rtc.begin()){
+    Serial.println("Nao foi possivel encontrar o RTC.");
+    while(1);
+  }
+
+  int novoAno;
+  int novoMes;
+  int novoDia;
+
+  Serial.println();
+  Serial.println("===== AJUSTE DE DATA =====");
+
+  novoAno = lerInteiro("Digite o ano:");
+  novoMes = lerInteiro("Digite o numero do mes:");
+  novoDia = lerInteiro("Digite o numero do dia:");
+
+  if(novoAno % 4 != 0 && novoMes == 2 && novoDia >= 28){
+    Serial.println("Valores inválidos.");
+    return;
+  }
+}
+
+void Menu::ajustarHora(){  //case 3
+  while (!Serial) {}
+
+  Serial.println();
+  Serial.println("===== SISTEMA RTC =====");
+
+  if (!rtc.begin()) {
+    Serial.println("Nao foi possivel encontrar o RTC.");
+    while (1);
+  }
+
+  int novaHora;
+  int novoMin;
+  int novoSeg;
+
+  Serial.println();
+  Serial.println("===== AJUSTE DE HORA =====");
+
+  novaHora = lerInteiro("Digite a hora (0-23):");
+  novoMin  = lerInteiro("Digite os minutos (0-59):");
+  novoSeg  = lerInteiro("Digite os segundos (0-59):");
+
+  if (novaHora < 0 || novaHora > 23 ||
+      novoMin  < 0 || novoMin  > 59 ||
+      novoSeg  < 0 || novoSeg  > 59) {
+
+    Serial.println("Valores invalidos.");
+    return;
+  }
+
+  DateTime agora = rtc.now();
+
+  rtc.adjust(DateTime(
+    agora.year(),
+    agora.month(),
+    agora.day(),
+    novaHora,
+    novoMin,
+    novoSeg
+  ));
+
+  Serial.println();
+  Serial.println("RTC ajustado com sucesso!");
 }
--- Menu.h
+++ Menu.h
@@ -5,6 +5,14 @@ class Menu {
   public:
     void mostrarMenu();
     void execOpcao(int option);
+
+    void mostrarHora();
+    void mostrarData();
+
+    int lerInteiro(const char* mensagem);
+
+    void ajustarHora();
+    void ajustarData();
 };
 
 #endif
